3db7a05364ad1ba8ff9ac3ba498bc8a64b1a83e3
ArinaBalakina <Balakinaarina@yandex.ru>
2023-11-09 11:40:06 +0500

    RouteAndError

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,15 @@
 import MainPage from '../../pages/main-page/main-page';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import SignInPage from '../../pages/signIn-page/signIn-page';
+import MyListPage from '../../pages/myList-page/myList-page';
+import {AppRoute, AuthorizationStatus} from '../const';
+import {HelmetProvider} from 'react-helmet-async';
+import PrivateRoute from '../privateRoute-page/priveteRoute-page';
+import MoviePage from '../../pages/movie-page/movie-page';
+import AddReviewPage from '../../pages/addReview-page/addReview-page';
+import PlayerPage from '../../pages/player-page/player-page';
+import Error404 from '../../pages/error-page/error404';
+
 
 type AppProps = {
   filmTitle: string;
@@ -6,9 +17,29 @@ type AppProps = {
   filmYear: number;
 }
 
+
 function App(props: AppProps): JSX.Element {
   return (
-    <MainPage {...props} />
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={AppRoute.Main} element={<MainPage {...props} />}/>
+          <Route path={AppRoute.SignIn} element={<SignInPage />}/>
+          <Route
+            path={AppRoute.MyList}
+            element={
+              <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+                <MyListPage />
+              </PrivateRoute>
+            }
+          />
+          <Route path={AppRoute.Film} element={<MoviePage/>} />
+          <Route path={AppRoute.AddReview} element={<AddReviewPage/>} />
+          <Route path={AppRoute.Player} element={<PlayerPage/>} />
+          <Route path={AppRoute.Error} element={<Error404/>} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -5,7 +5,7 @@ type LogoProps = {
 function Logo({isLight}:LogoProps): JSX.Element {
   return (
     <div className="logo">
-      <a className={`logo__link ${isLight ? 'logo__link--light' : ''}`}>
+      <a href="/" className={`logo__link ${isLight ? 'logo__link--light' : ''}`}>
         <span className="logo__letter logo__letter--1">W</span>
         <span className="logo__letter logo__letter--2">T</span>
         <span className="logo__letter logo__letter--3">W</span>
