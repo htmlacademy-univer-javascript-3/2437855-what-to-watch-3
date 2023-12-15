bef8f21548a85231c18e73da46e9f29842a53d9d
ArinaBalakina <Balakinaarina@yandex.ru>
2023-12-16 00:01:47 +0500

    myList

--- src/pages/myList-page/myList-page.tsx
+++ src/pages/myList-page/myList-page.tsx
@@ -1,27 +1,26 @@
 import Logo from '../../components/logo/logo';
 import Footer from '../../components/footer/footer';
 import FilmsList from '../../components/filmList/filmList';
-import { Films } from '../../types/film';
 import User from '../../components/user/user';
+import { useAppSelector } from '../../hook/useAppDispatch';
+import { getMyList } from '../../store/myList-reducer/myList-selector';
 
-type MyListPageProps = {
-  films: Films;
-};
+function MyListPage(): JSX.Element {
+  const myList = useAppSelector(getMyList);
 
-function MyListPage({ films }: MyListPageProps): JSX.Element {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
         <Logo isLight />
 
         <h1 className="page-title user-page__title">
-          My list <span className="user-page__film-count">9</span>
+          My list<span className="user-page__film-count">{myList.length}</span>
         </h1>
         <User />
       </header>
       <section className="catalog">
         <h2 className="catalog__title visually-hidden">Catalog</h2>
-        <FilmsList films={films} />
+        <FilmsList films={myList} genre={'All Genres'} />
       </section>
       <Footer />
     </div>
